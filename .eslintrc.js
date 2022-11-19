90811c5b4117904c1721c6cc54eb0804a508a134
Wallace Choba Ferrante <wallaceferrante@hotmail.com>
2022-11-19 08:14:10 -0300

    feat: added eslint and vscode folder

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true,
+		jest: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'./tsconfig.json'
+	],
+	include: [
+		'babel.config.js'
+	],
+	overrides: [
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		'project': './tsconfig.json',
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	plugins: [
+		'react',
+		'@typescript-eslint'
+	],
+	rules: {
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'quotes': ['error', 'single', { avoidEscape: true }],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+	},
+	settings: {
+    react: {
+      version: 'detect',
+    },
+  }
+}
